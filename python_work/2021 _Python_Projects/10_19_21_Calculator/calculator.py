b7a3c7f64564fbc7068368841e0c48d60ed719ff
Robert Martin <robert.martin92@protonmail.com>
2021-10-24 04:02:27 -1000

    Initial Commit

---
+++ python_work/2021 _Python_Projects/10_19_21_Calculator/calculator.py
@@ -0,0 +1,11 @@
+#Date: October 19, 2021
+#Scope: Build Calculator
+#Resource: https://www.youtube.com/watch?v=sY6MWch52Oo
+#
+message = "Hello Python World!"
+print(message)
+
+hta = "Cyber Class"
+class_time = "Done"
+sent = f"{hta} {class_time}"
+print (sent)
