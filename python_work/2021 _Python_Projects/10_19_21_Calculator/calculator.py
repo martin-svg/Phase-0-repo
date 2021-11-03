86f6e5f39205b8a02dbd0768084b3110f3fe9f89
Robert Martin <robert.martin92@protonmail.com>
2021-11-02 21:25:50 -1000

    Added Lambda inline function

--- python_work/2021 _Python_Projects/10_19_21_Calculator/calculator.py
+++ python_work/2021 _Python_Projects/10_19_21_Calculator/calculator.py
@@ -1,11 +1,35 @@
-# Date: October 26, 2021
+# Date: November 1, 2021
 # Scope: Build Calculator
 # Resource: https://www.youtube.com/watch?v=sY6MWch52Oo
-#
-message = "Hello Python World!"
-print(message)
+# Topics:tkinter, grid geometry manager, eval, try/except, lambda
 
-hta = "Cyber Class"
-class_time = "Done"
-sent = f"{hta} {class_time}"
-print(sent)
+import tkinter
+
+root = tkinter.Tk()
+root.title("Calculator")
+# Create functions
+def add(value):
+    print(value)
+
+
+# Create Gui, think about what kind of buttons to make
+# using columns, rows, and span.
+label_result = tkinter.Label(root, text="")
+label_result.grid(row=0, column=0, columnspan=4)
+
+button_1 = tkinter.Button(root, text="1", command=lambda: add("1"))
+button_1.grid(row=1, column=0)
+
+button_2 = tkinter.Button(root, text="2", command=lambda: add("2"))
+button_2.grid(row=1, column=1)
+
+button_3 = tkinter.Button(root, text="3", command=lambda: add("3"))
+button_3.grid(row=1, column=2)
+
+button_divide = tkinter.Button(root, text="/", command=lambda: add("/"))
+button_divide.grid(row=1, column=3)
+
+
+# This is how I am closing the main loop of the program.
+# If I don't have this the window will shut automatically.
+root.mainloop()
