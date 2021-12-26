ee2ac78b0f66efc610cb175aab3dcc332da8bcba
Robert Martin <robert.martin92@protonmail.com>
2021-12-26 13:09:32 -1000

    created docker_work folder

---
+++ docker_work/docker_prj_1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This is a project to learn containerization,
+# the website I am using is from the docker-curriculum.
+
+echo " This tutorial builds and deploys a webapp in
+the cloud. It uses AWS to deploy a static website,
+and two dynamic webapps on EC2 using Elastic Beanstalk
+and Elastic Container Service. "
+
