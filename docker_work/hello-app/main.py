4d61389c7b2be56d9915d05241cd0e59ee7f1a10
Robert Martin <robert.martin92@protonmail.com>
2022-01-02 13:16:56 -1000

    complete python app and deployment using GCP App Engine

--- docker_work/hello-app/main.py
+++ docker_work/hello-app/main.py
@@ -4,14 +4,48 @@
 # https://stackoverflow.com/questions/31252791/flask-importerror-no-module-named-flask
 
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "<h1>Testing<h1>"
+    celsius = request.args.get("celsius", "")
+    if celsius:
+        fahrenheit = fahrenheit_from(celsius)
+    else:
+        fahrenheit = ""
+    return (
+        """<form action="" method="get">
+                Celsius temperature: <input type="text" name="celsius">
+                <input type="submit" value="Conversion">
+                </form>"""
+        # + "Fahrenheit: "
+        + fahrenheit
+    )
 
+
+# @app.route("/<int:celsius>")
+# Adding int: before the variable name tells Flask to check
+# whether the input it receives from the URL can be converted to an integer.
+# If it can, then the content is passed on to fahrenheit_from().
+# If it can’t, then Flask displays a Not Found error page.
+
+
+def fahrenheit_from(celsius):
+    """Convert Celsius to Fahrenheit degrees."""
+    try:
+        fahrenheit = float(celsius) * 9 / 5 + 32
+        fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
+        return str(fahrenheit)
+    except ValueError:
+        return "invalid input"
+
+
+# if __name__ == "__main__":
+#    celsius = input("Celsius: ")
+#   print("Fahrenheit:", fahrenheit_from(celsius))
 
 if __name__ == "_main_":
     app.run(host="127.0.0.1", port=8080, debug=True)
---
+++ docker_work/temp-cvt-app/tmp-cvt-app.py
@@ -0,0 +1,16 @@
+# Example of a temerature converter sciprt
+
+
+def fahrenheit_from(celsius):
+    """Convert Celsius to Fahrenheit degrees."""
+    try:
+        fahrenheit = float(celsius) * 9 / 5 + 32
+        fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
+        return str(fahrenheit)
+    except ValueError:
+        return "invalid input"
+
+
+if __name__ == "__main__":
+    celsius = input("Celsius: ")
+    print("Fahrenheit:", fahrenheit_from(celsius))
