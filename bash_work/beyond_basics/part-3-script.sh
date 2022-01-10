3770c5e16ea66df87102fb564b0704207e2fd13c
Robert Martin <robert.martin92@protonmail.com>
2022-01-09 21:36:16 -1000

    Complete section 9.1 in Bash script book.

--- bash_work/beyond_basics/part-3-script.sh
+++ bash_work/beyond_basics/part-3-script.sh
@@ -45,3 +45,85 @@ echo $var3
 # $LINEN0 - this variable is the line number of the shell script in which 
 # this variable appears. 
 
+# Begin Debug block
+last_cmd_arg=$_
+
+echo "At line number $LINE0, variable \"v1\" = $v1"
+echo "Last command argument processed = $last_cmd_arg"
+# End debug block
+
+echo
+echo $MACHTYPE
+
+echo
+echo $OSTYPE
+
+ehco
+echo $PATH # Path to binaries. The path to environment variable is
+# normally stored in /etc/provile or ~/.bashrc
+
+# PATH=${PATH}:/opt/bin appends the /opt/bin directory to the 
+# current path. In a script, it may be expedient to temporarily 
+# add a directory to the path in this way. 
+# When the script exits, this restores the original 
+# $PATH (a child process, 
+# such as a script, may not change the environment of the 
+# parent process, the shell).
+
+echo
+echo $BASH_VERISON
+
+echo
+echo $PPID # The PPID f a process is the process ID (pid) of its parent
+
+# If $TMOUT environmental variable is set to a non-zero value time,
+# then the shell prompt will time out after $time seconds, this will
+# cause a logout
+
+echo
+TMOUT=3
+
+echo "What is your favorite food?"
+echo "Quickly now, you only have $TMOUT seconds to answer!"
+read food
+
+if [ -z "$food" ]
+then
+ song="(no answer)"
+ #defaule response
+fi
+
+echo "Your favorite food is $food."
+echo 
+
+echo $UID # User ID number
+
+
+# Positional Parameters
+
+# $0, $1, $3, etc are positonal parameters, passed from command line
+# script, passed to a funciton or set to a variable. 
+
+# $# is the number of command-line arguments or positional parameters
+
+# $* are all of the positional parameters, seen as a single word
+
+# $! will list the PID (process ID) of last job run in background
+
+# $_ is a special varialbe set to final argument of previous command
+
+du >/dev/null
+echo $_
+
+ls -al >/dev/null
+echo $_
+
+:
+echo $_
+
+
+echo
+
+# $? is the exit status of a command, function, or the script itself
+
+# $$ is the PID (Process ID_ of the script itself. 
