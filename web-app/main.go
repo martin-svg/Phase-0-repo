c5ca0c656cfc012004272e5f3857d38919c96871
Robert Martin <robert.martin92@protonmail.com>
2022-05-07 12:34:15 -1000

    just learning stuff

---
+++ web-app/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"github.com/GoesToEleven/Go_Training/package/stringutil"
+)
+
+func main (){
+	// Print Hello World
+	fmt.Println("Hello World!")
+	fmt.Println(winniepooh.BearName)
+
+	// For the variable G, initialize at 1. When the variable G is 
+	// less then 10, add 1 to  the variable G. Print the Output as a 
+	// Decimal format. 
+	for g := 1; g < 10; g++ {
+		fmt.Printf("%d \t", g)
+	}
+
+
+
+
+}
